cf163e9eb758b79508b9bbf5d5f21fb5109e57d8
Durand Sinclair <durand.sinclair@gmail.com>
2020-10-09 18:37:13 +1100

    Adds add_two function with tests

---
+++ R/add_two.R
@@ -0,0 +1,15 @@
+#' add_two
+#'
+#' @param a An integer
+#' @param b Another integer
+#'
+#' @return the sum of the two integers
+#' @export
+#'
+#' @examples
+#' sum(1, 2)
+#' 3
+add_two <- function(a, b) {
+    result <- sum(a, b)
+    result
+}
